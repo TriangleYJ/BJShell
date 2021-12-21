dbc9470261499c56940c6c91b2c525ff4c80faf7
Triangleyj <dpwns99@gmail.com>
2021-12-21 18:42:17 +0900

    Implement submitter class, update to handle recapcha manually

--- app.js
+++ app.js
@@ -1,65 +1,113 @@
 import dotenv from 'dotenv'
-import axios from 'axios'
 import puppeteer from 'puppeteer'
 dotenv.config()
 
-const langs = {0: 'C99', 12: 'Go', 17: 'node.js', 28: 'Python 3', 29: 'D', 58: 'Text', 68: 'Ruby', 69: 'Kotlin (JVM)', 73: 'PyPy3', 74: 'Swift', 84: 'C++17', 85: 'C++17 (Clang)', 86: 'C# 9.0 (.NET)', 93: 'Java 11', 94: 'Rust 2018'}
-
-const qnum = "2557"
-const lang_id = 17
-const ansjs = 'print("Hello World!")'
-const ans = 'console.log("Hello World!")'
+const langs = {
+    'C99': '0',
+    'Go': '12',
+    'node.js': '17',
+    'Python 3': '28',
+    'D': '29',
+    'Text': '58',
+    'Ruby': '68',
+    'Kotlin (JVM)': '69',
+    'PyPy3': '73',
+    'Swift': '74',
+    'C++17': '84',
+    'C++17 (Clang)': '85',
+    'C# 9.0 (.NET)': '86',
+    'Java 11': '93',
+    'Rust 2018': '94'
+  }
 
 //TODO: recapcha problem
 //TODO: execute in runtime
 
-const myloc = async(qnum, lang_id, ans) => {
-    try{
-        const browser = await puppeteer.launch({headless: false})
-        const page = await browser.newPage()
-        await page.goto('https://www.acmicpc.net/login?next=%2F')
-        page.waitForSelector('form')
-        await page.evaluate((id, pw) => {
-            document.querySelector('input[name="login_user_id"]').value = id
-            document.querySelector('input[name="login_password"]').value = pw
-            document.querySelector('#submit_button').click()
-        }, process.env.BJ_ID, process.env.BJ_PW)
-        
-        await page.waitForTimeout(10000)
-        await page.goto('https://www.acmicpc.net/submit/'+qnum)
-        await page.waitForSelector('form')
-        await page.evaluate((lang, code) => {
-            document.querySelector('#language').value = lang
-            document.querySelector('.CodeMirror').CodeMirror.setValue(code)
-            document.querySelector('#submit_button').click()
-        }, lang_id, ans)
-        await page.waitForTimeout(1000)
-        let finish = false
-
-        console.log("Code submited.")
-        console.log("=== #" + qnum + " Evaluation Result ===")
-
-
-        while(!finish){
-            await page.waitForTimeout(1000)
-            let element = await page.$('td.result > span')
-            let res = await page.evaluate(el => el.textContent, element)
-            if(!res) break;
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);
-            process.stdout.write(res); // end the line
-            if(res == '기다리는 중' || res.includes('채점')) finish = false
-            else finish = true
-        }
-        await browser.close()
-
-    } catch(err) {
-        console.log(err)
-        await browser.close()
+class Submitter {
+    constructor (id, pw){
+        this.id = id
+        this.pw = pw
+        this.browser = null
     }
-};
+    
+    async login() {
+        try{
+            this.browser = await puppeteer.launch({headless: false})
+            this.page = await this.browser.newPage()
+            await this.page.goto('https://www.acmicpc.net/login?next=%2F')
+            this.page.waitForSelector('form')
+            await this.page.evaluate((id, pw) => {
+                document.querySelector('input[name="login_user_id"]').value = id
+                document.querySelector('input[name="login_password"]').value = pw
+                document.querySelector('#submit_button').click()
+            }, process.env.BJ_ID, process.env.BJ_PW)
+
+            await this.page.waitForNavigation()
+            let cookies = await this.page.cookies()
+            await this.browser.close()
+
+            this.browser = await puppeteer.launch({headless: true})
+            this.page = await this.browser.newPage()
+            await this.page.setCookie(...cookies)
+        } catch (err) {
+            console.log(err)
+            await this.logout()
+        }
+    }
+
+    async logout() {
+        await this.browser.close()
+        this.browser = null
+        this.page = null
+    }
+
+    async submit(qnum, lang_str, ans){
+        try{
+            await this.page.goto('https://www.acmicpc.net/submit/'+qnum)
+            await this.page.waitForTimeout(1000)
+            await this.page.waitForSelector('form')
+            await this.page.evaluate((lang, code) => {
+                document.querySelector('#language').value = lang
+                document.querySelector('.CodeMirror').CodeMirror.setValue(code)
+                document.querySelector('#submit_button').click()
+            }, langs[lang_str], ans)
+
+            await this.page.waitForTimeout(1000)
+            let finish = false
+            console.log("")
+            console.log("Code submited.")
 
 
-let str = ``
-myloc(15990, 28, str);
+            while(!finish){
+                await this.page.waitForTimeout(1000)
+                let element = await this.page.$('td.result > span')
+                let res = await this.page.evaluate(el => el.textContent, element)
+                if(!res) break;
+                process.stdout.clearLine();
+                process.stdout.cursorTo(0);
+                process.stdout.write("[#" + qnum + "] " + res); // end the line
+                if(res == '기다리는 중' || res.includes('채점')) finish = false
+                else finish = true
+            }
+        } catch (err) {
+            console.log(err);
+            await this.logout()
+        }
+    }
+    
+}
 
+
+//let str = ``
+
+const ans1 = ``;
+
+
+(async () => {
+    const mySubmittor = new Submitter(process.env.BJ_ID, process.env.BJ_PW)
+    await mySubmittor.login();
+    await mySubmittor.submit("2557", "node.js", 'console.log("Hello World!")')
+    await mySubmittor.submit('15990', "Python 3", ans1)
+    await mySubmittor.submit("2557", "Python 3", 'print("Hello World!")')
+    await mySubmittor.logout();
+})();
